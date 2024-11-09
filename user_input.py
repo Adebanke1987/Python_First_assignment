4ab4187f1b8e83492877b6f12a27fa0d67bfa6d4
Adebanke1987 <akinyemilara@gmail.com>
2024-11-09 17:27:34 +0100

    Python Assignment

---
+++ user_input.py
@@ -0,0 +1,17 @@
+ # Use the input() function to ask the user for their name and store it in a variable called "name". 
+name = "My name is Victoria" 
+print(name)  
+
+# Use the input() function to ask the user for their age and store it in a variable called "age"
+age = 38
+print(age)
+
+# Use the input() function to ask the user for their location and store it in a variable called "location"
+location = "Lagos", "Nigeria"
+print(location)
+
+# Print out a personalized message using the user's name, age, and location. For example: "Hello [name], you are [age] years old and live in [location]."
+name, age, location = "Hello Victoria", "I am 38 years old", "and live in Lagos, Nigeria"
+print(name)
+print(age)
+print(location)
